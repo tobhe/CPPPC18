1bb5e317ec971e56266cb18b46e58d328dcd9acd
lalala <lalala>
2018-05-21 18:34:36 +0200

    added Histogram

--- a-04/solution/histogram.h
+++ a-04/solution/histogram.h
@@ -7,17 +7,27 @@
 #include <set>
 #include <unordered_map>
 
-template <class Iter> inline Iter histogram(Iter first, Iter last) {
+template <class Iter>
+inline Iter histogram(Iter first, Iter last) {
   typedef typename std::iterator_traits<Iter>::value_type value_t;
-  typedef typename std::iterator_traits<Iter>::difference_type index_t;
+  typedef typename std::iterator_traits<Iter>::difference_type index_t __attribute__ ((used));
 
-  std::unordered_map<index_t, value_t> map{};
-  std::transform(first, last, [map, first, last](value_t &in) {
-    map.insert({in, std::count(first, last, in)});
-    return in;
+  if (first == last ) {
+    return first;
+  }
+
+  std::unordered_map<value_t, size_t> map;
+
+  auto new_end = std::remove_if(first, last, [&map, first, last](value_t &in) {
+      if (map.find(in) == map.end()) {
+        map.insert({in, std::count(first, last, in)});
+        return false;
+      }
+      return true;
   });
-  Std::remove_if(first, last, true);
-  return std::copy(map.begin(), map.end(), first);
+
+  std::for_each(first, new_end, [&map](value_t &in){ in = map[in]; });
+  return new_end;
 }
 
 #endif // CPPPC__S03__HISTOGRAM_H__INCLUDED
